38fc20acb43400176858af89ec960d46f283538a
dipesh_teksewa <dipesh_teksewa@DESKTOP-5P2I522>
2023-06-01 16:52:38 +0545

    Add project files.

---
+++ newcrud/Modle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace newcrud
+{
+    public class Modle
+    {
+        public int pid { get; set; }
+        public string pname { get; set; }
+        public string city { get; set; }
+    }
+}
---
+++ newcrud/ProductMap.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace newcrud
+{
+    public class ProductMap : ClassMap<Modle>
+    {
+        public ProductMap()
+        {
+            Id(x => x.pid);
+
+            Map(x => x.pname);
+
+            Map(x => x.city);
+
+            Table("Model");
+
+        }
+    }
+}
