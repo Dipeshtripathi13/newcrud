e3a38ff56b2548a9804aa66dd218ff7baa9f6019
Dipeshtripathi13 <killredps@gmail.com>
2023-07-25 09:30:54 +0545

    fhfhh

--- newcrud/Default.aspx.cs
+++ newcrud/Default.aspx.cs
@@ -42,7 +42,7 @@ namespace newcrud
         //        RadGrid1.DataSource = models;
         //        RadGrid1.DataBind();
         //    }
-        //}dfdgdhjfhfhgjgghjghg
+        //}dfdgdhjfhfhgjgghjghgljkkh
 
         private void BindRadGrid()
         {
